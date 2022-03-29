6b53d9e63fedd089265ab78795ff0a60d24d5c9d
PauloDelphos <72270551+paulodelphos@users.noreply.github.com>
2022-03-28 21:44:00 -0300

    Add files via upload

---
+++ exercicios002/ex002-CPP/ex002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string nome;
+
+int main() {
+
+    cout << "\nDigite o seu nome: ";
+    cin >> nome;
+    cout << "Eh um prazer te conhecer " + nome + "!\n";
+
+    return 0;
+}
---
+++ exercicios002/ex002-Csharp/ex002.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+        static void Main() {
+        String nome;
+
+        Console.Write("\nDigite o seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Muito prazer tem te conhecer " + nome);   
+    }
+}
